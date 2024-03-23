99b244ab144036922360337e8b24639d6f2fb36d
Tobias Kjær <tobias.hougs.kjaer@gmail.com>
2024-03-23 13:37:07 +0100

    Added Pie Model Class

--- BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
+++ BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b40b888d534bc6445698a2e689f20cb97e33faa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
