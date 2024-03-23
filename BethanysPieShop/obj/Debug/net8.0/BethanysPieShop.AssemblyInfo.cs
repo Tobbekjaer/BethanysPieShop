9d8d9f6e5e082f38aed4900749057804716f0b12
Tobias Kjær <tobias.hougs.kjaer@gmail.com>
2024-03-23 14:38:11 +0100

    Added ViewImports and ViewStart Views

--- BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
+++ BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86b1ca5b260cca3db1739d036658b7b4efa083e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09de07aebac724f6f4836dfce37c456c41c9e778")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
