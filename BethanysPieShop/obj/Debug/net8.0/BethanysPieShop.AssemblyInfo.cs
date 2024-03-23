70a14ae11fce23de177814da549f91bab5f37bb2
Tobias Kjær <tobias.hougs.kjaer@gmail.com>
2024-03-23 13:42:16 +0100

    Added IPieRepository og ICategoryRepository interfaces

--- BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
+++ BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b244ab144036922360337e8b24639d6f2fb36d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbfd47d744032e91f124eb44c9d5e304c2bf097a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
