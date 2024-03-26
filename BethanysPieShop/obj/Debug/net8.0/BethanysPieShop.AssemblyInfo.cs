2cd289df81e88050035fd5265fa5668c099c7ede
Tobias Kjær <tobias.hougs.kjaer@gmail.com>
2024-03-26 18:42:57 +0100

    Added ShoppingCart Index view and AddToCart buttons in index view and details view

--- BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
+++ BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4969ee168ed363b75000167b08b530b325a1e60f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423f39b343bf763e3ab6af85989ab39142624529")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
